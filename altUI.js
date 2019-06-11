01011a627c61d9dad7ceae41c5c6a80efec8b1d1
Eduardo Villalpando Mello <5952839+edvilme@users.noreply.github.com>
2019-06-11 13:34:40 -0500

    Update altUI.js

--- altUI.js
+++ altUI.js
@@ -1082,7 +1082,8 @@ UI.TabView.prototype.addTab = function(label, content){
 	let TabContent = document.createElement('div');
 	TabContent.classList.add('altUI_TabViewContent');
 	TabContent.id = label;
-	TabContent.innerHTML = content;
+	TabContent.innerHTML = '';
+	TabContent.append(content)
 	this.dom.querySelector('.altUI_TabViewTabs').append(TabLabel);
 	this.dom.querySelector('.altUI_TabViewViews').append(TabContent);
 	TabLabel.addEventListener('click', function(){t.setTab(label)});
