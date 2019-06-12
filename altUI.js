c2948cad5d497a618f16e630d4b44604b1ae089c
Eduardo Villalpando Mello <5952839+edvilme@users.noreply.github.com>
2019-06-11 20:55:06 -0500

    Update altUI.js

--- altUI.js
+++ altUI.js
@@ -1010,30 +1010,26 @@ UI.Modal = function ( value ) {
 
 	var scope = this;
 
-	var dom = document.createElement( 'div' );
+	this.dom = document.createElement( 'div' );
 
-	dom.style.position = 'absolute';
-	dom.style.width = '100%';
-	dom.style.height = '100%';
-	dom.style.backgroundColor = 'rgba(0,0,0,0.5)';
-	dom.style.display = 'none';
-	dom.style.alignItems = 'center';
-	dom.style.justifyContent = 'center';
-	dom.addEventListener( 'click', function ( event ) {
-
+	this.dom.style.position = 'absolute';
+	this.dom.style.width = '100%';
+	this.dom.style.height = '100%';
+	this.dom.style.backgroundColor = 'rgba(0,0,0,0.5)';
+	this.dom.style.display = 'none';
+	this.dom.style.alignItems = 'center';
+	this.dom.style.justifyContent = 'center';
+	this.dom.addEventListener( 'click', function ( event ) {
 		scope.hide();
-
 	} );
-
-	this.dom = dom;
-
+	
 	this.container = new UI.Panel();
 	this.container.dom.style.width = '200px';
 	this.container.dom.style.padding = '20px';
 	this.container.dom.style.backgroundColor = '#ffffff';
 	this.container.dom.style.boxShadow = '0px 5px 10px rgba(0,0,0,0.5)';
 
-	this.add( this.container );
+	this.dom.append( this.container );
 
 	return this;
 
