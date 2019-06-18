e76f22d51c0b503520ff916af556efaa8ff27ad2
Eduardo Villalpando Mello <5952839+edvilme@users.noreply.github.com>
2019-06-18 14:18:28 -0500

    Update altUI.js

--- altUI.js
+++ altUI.js
@@ -280,7 +280,7 @@ UI.InputView = function (hint, value, name, type) {
 	
 	this.input = null;
 	if(type='button'){
-		this.input = new UI.Button(hint);
+		this.input = new UI.Button(hint).dom;
 	}else{
 		this.input = document.createElement( 'input' );
 		this.input.type = type;			
