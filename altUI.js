f61a677c4edda6c120e14a6ca5bb4666a212dc41
Eduardo Villalpando Mello <5952839+edvilme@users.noreply.github.com>
2019-08-02 18:54:20 -0500

    Update altUI.js

--- altUI.js
+++ altUI.js
@@ -1740,6 +1740,7 @@ UI.Chart.Percentage = function(data){
 		let section = document.createElement('div');
 		section.classList.add('altUI_Chart_Percentage_Data');
 		section.style.width = Math.floor(data[i]*100/all)+'%';
+		scope.append(section);
 	});
 	return this;
 }
